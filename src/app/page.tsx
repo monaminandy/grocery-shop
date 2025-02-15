10b5c03608443b7fe73af54c21a0421ce4613831
monami_nandy <monaminandy16@gmail.com>
2025-02-15 10:22:07 +0530

    Hero Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-
+import  Hero  from "../components/Hero" ;
 
 export default function Home() {
   return (
     <>
     <div className="mt-80 mb-80 text-secondary font-bold text-center text-4xl">
-      <p>Coming Soon</p>
+      <Hero />
     </div>
     </>
   );
---
+++ src/components/Hero.tsx
@@ -0,0 +1,102 @@
+"use client";
+import React from "react";
+import { HeroParallax } from "../components/ui/hero-parallax";
+
+export default function Hero() {
+  return <HeroParallax products={products} />;
+}
+export const products = [
+  {
+    title: "Grains",
+    link: "https://gomoonbeam.com",
+    thumbnail:
+      "/grocery1.png",
+  },
+  {
+    title: "Vegetables",
+    link: "https://cursor.so",
+    thumbnail:
+      "/grocery2.png",
+  },
+  {
+    title: "Grocery",
+    link: "https://userogue.com",
+    thumbnail:
+      "/grocery3.png",
+  },
+
+  {
+    title: "Rice Field",
+    link: "https://editorially.org",
+    thumbnail:
+      "/grocery4.png",
+  },
+  {
+    title: "Wheat Field",
+    link: "https://editrix.ai",
+    thumbnail:
+      "/grocery5.png",
+  },
+  {
+    title: "Mustard Field",
+    link: "https://app.pixelperfect.quest",
+    thumbnail:
+      "/grocery6.png",
+  },
+
+  {
+    title: "Potato",
+    link: "https://algochurn.com",
+    thumbnail:
+      "/grocery7.png",
+  },
+  {
+    title: "Carrot",
+    link: "https://ui.aceternity.com",
+    thumbnail:
+      "/grocery8.png",
+  },
+  {
+    title: "Ginger",
+    link: "https://tailwindmasterkit.com",
+    thumbnail:
+      "/grocery9.png",
+  },
+  {
+    title: "Garlic",
+    link: "https://smartbridgetech.com",
+    thumbnail:
+      "/grocery10.png",
+  },
+  {
+    title: "Cabbage",
+    link: "https://renderwork.studio",
+    thumbnail:
+      "/grocery11.png",
+  },
+
+  {
+    title: "Wheat",
+    link: "https://cremedigital.com",
+    thumbnail:
+      "/grocery12.png",
+  },
+  {
+    title: "Field",
+    link: "https://goldenbellsacademy.com",
+    thumbnail:
+      "/grocery13.png",
+  },
+  {
+    title: "Rice",
+    link: "https://invoker.lol",
+    thumbnail:
+      "/grocery14.png",
+  },
+  {
+    title: "Onions And Carrots",
+    link: "https://efreeinvoice.com",
+    thumbnail:
+      "/grocery15.png",
+  },
+];
